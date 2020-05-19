6da864b95d37ff5750acb8c3ea94c7de26f360d9
Carollo Federico <f.carollo.0729@vallauri.edu>
2020-05-19 16:55:05 +0200

    modifiche minori objMex

--- BlackJack_Server/ObjMex.cs
+++ BlackJack_Server/ObjMex.cs
@@ -16,28 +16,11 @@ namespace BlackJack_Server
         public List<dynamic> MultipleData { get => _multipleData; set => _multipleData = value; }
         public dynamic SingleData { get => _singleData; set => _singleData = value; }
 
-        //Quando si passano più valori
-        public ObjMex(string action, List<dynamic> data)
-        {
-            this._action = action;
-            this._multipleData = data;
-        }
-        //Quando si passa un singolo valore
-        public ObjMex(string action, dynamic data)
-        {
-            this._action = action;
-            this._singleData = data;
-        }
-        public ObjMex(string action)
-        {
-
-        }
-
         public ObjMex(string action, List<dynamic> multipleData, dynamic singleData)
         {
             this._action = action;
             this._singleData = singleData;
-            this.MultipleData = multipleData;
+            this._multipleData = multipleData;
         }
 
         public ObjMex() { }
